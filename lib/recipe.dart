037e16ff8d163860fe60329114e0386a13d6c588
assa19@yahoo.com <assa19@yahoo.com>
2022-04-20 01:14:16 +0100

    first commit

---
+++ lib/recipe.dart
@@ -0,0 +1,76 @@
+
+class Recipe{
+
+    int serving;
+    List<Ingredient> ingredients;
+
+    String label;
+    String imageUrl;
+
+    Recipe(this.label, this.imageUrl, this.serving, this.ingredients);
+    static List<Recipe> samples = [
+        Recipe('Spaghetti and Meatballs', 'assets/first.jpg',
+        4,
+        [
+        Ingredient(1, 'box', 'Spaghetti'),
+        Ingredient(4, '', 'Frozen Meatballs'),
+        Ingredient(0.5, 'jar', 'sauce'),
+        ],
+        ),
+        Recipe(
+            'Tomato Soup','assets/two.jpg',
+            2,
+            [
+                Ingredient(1, 'can', 'Tomato Soup'),
+            ],
+        ),
+        Recipe(
+            'Grilled Cheese',
+            'assets/three.jpg',
+            1,
+            [
+                Ingredient(2, 'slices', 'Cheese'),
+                Ingredient(2, 'slices', 'Bread'),
+            ],
+        ),
+        Recipe(
+            'Chocolate Chip Cookies',
+            'assets/four.jpg',
+            24,
+            [
+                Ingredient(4, 'cups', 'flour'),
+                Ingredient(2, 'cups', 'sugar'),
+                Ingredient(0.5, 'cups', 'chocolate chips'),
+            ],
+        ),
+        Recipe(
+            'Taco Salad',
+            'assets/five.jpg',
+            1,
+            [
+                Ingredient(4, 'oz', 'nachos'),
+                Ingredient(3, 'oz', 'taco meat'),
+                Ingredient(0.5, 'cup', 'cheese'),
+                Ingredient(0.25, 'cup', 'chopped tomatoes'),
+            ],
+        ),
+        Recipe(
+            'Hawaiian Pizza',
+            'assets/six.jpg',
+            4,
+            [
+                Ingredient(1, 'item', 'pizza'),
+                Ingredient(1, 'cup', 'pineapple'),
+                Ingredient(8, 'oz', 'ham'),
+            ],
+        ),
+    ];
+}
+
+class Ingredient{
+    double quantity;
+    String measure;
+    String name;
+
+    Ingredient(this.quantity, this.measure, this.name);
+}
